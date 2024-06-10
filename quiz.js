ff0a03e3760c83b91ead33da1d9d0f7347a21e70
Alizamankhan786 <az030366@gmail.com>
2024-06-10 14:10:56 -0700

    index file updated

--- quiz.js
+++ quiz.js
@@ -1,10 +1,19 @@
 const div = document.querySelector(`#quiz-item`);
 const btn = document.querySelector(`#btn`);
-
-let index = 0
+let index = 0;
+let result = 0;
+let totalMarks = 0;
 let questionArr = [];
 
+function shuffleArray(Array){
+    for (let i = Array.length -1; i > 0 ; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [Array[i] , Array[j]] = [Array[j] , Array[i]];
+    }
 
+    return Array;
+
+}
 
 const renderQuestion = (arr) =>{
     if(index < questionArr.length){
@@ -17,21 +26,42 @@ const renderQuestion = (arr) =>{
         <h3 class="text-light"> Q${index + 1}:  ${arr[index].question.text}</h3>
 
         <ul class="text-light">
-        ${answerArr.map((items) => `<li>${items}</li>`)}
+        ${shuffleArray(answerArr).map((items) => `<li>
+            
+            <input type="checkbox" name="choice" class="choice" id="${items} value=${items}>
+
+            <label for = ${items}>${items}</label>
+
+            </li>`
+        )}
         </ul>
-        `
+        `;
     }else{
-        console.log(`Questions Completed`);
+        console.log(`Show Result`);
+        Window.location = "result.html";
+        localStorage.setItem(
+            "result",
+            JSON.stringify({
+                totalMarks,
+                result,
+            })
+        );
     }
 };
 
 btn.addEventListener(`click` , ()=>{
+    const choice = document.querySelectorAll(`.choice`);
     div.innerHTML = ``;
+    choice.forEach((item) =>{
+        if(item.checked){
+            if(item.nextSibling.innerHTML === questionArr[index].correctAnswers){
+                result += 10;
+            }
+        }
+    });
     index += 1;
     renderQuestion(questionArr);
 });
-
-
 
 
 const getQuestion  = async () => {
@@ -39,7 +69,8 @@ const getQuestion  = async () => {
         const data = await fetch(`https://the-trivia-api.com/v2/questions`);
         const response = await data.json();
         console.log(response);
-        questionArr = (response);
+        totalMarks = response.length * 10;
+        questionArr = response;
         renderQuestion(response);
         
     } catch (error) {
